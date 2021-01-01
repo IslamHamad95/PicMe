cab4e4c13b6bc0ac59b1fd60697ad98234dadd9d
IslamHamad <islam.elnil95@gmail.com>
2021-01-01 20:03:41 +0200

    fixing errors

--- src/Matrial-ui/imports.js
+++ src/Matrial-ui/imports.js
@@ -2,9 +2,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Container from "@material-ui/core/Container";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
-import FormControl from '@material-ui/core/FormControl'
-import Input from "@material-ui/core/input"
-import TextField from "@material-ui/core/TextField"
+import {Input} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -15,4 +14,4 @@ import Paper from '@material-ui/core/Paper';
 import { fade, makeStyles } from "@material-ui/core/styles";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
-export { AppBar,Container,Input,Grid,Paper,FormControl, Toolbar, IconButton, Typography, InputLabel, fade, makeStyles, MenuIcon,SearchIcon,Button, TextField };
+export { AppBar,Container,Input,Grid,Paper, Toolbar, IconButton, Typography, InputLabel, fade, makeStyles, MenuIcon,SearchIcon,Button, TextField };
